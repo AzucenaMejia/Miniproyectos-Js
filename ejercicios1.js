b647304b58e35a5a5cdb16db371afdfe18ee15a1
AzucenaMejia <blazu.mejia11@gmail.com>
2026-02-19 15:12:18 -0500

    Recreate HTML structure for the ball game project and remove obsolete files

--- ejercicios1.js
+++
@@ -1,8 +0,0 @@
-let Saludo = "¡Hola a todos!";
-console.log(Saludo);
-
-const descripcion = "Este es un ejercicio de JavaScript para practicar la sintaxis básica.";
-console.log(descripcion);
-
-alert("¡Bienvenido al ejercicio de JavaScript!")
-

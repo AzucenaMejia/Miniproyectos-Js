dd7e60476920c9f1c043dde77713c6135e3d462c
AzucenaMejia <blazu.mejia11@gmail.com>
2026-05-07 15:53:05 -0500

    Add HTML file and JavaScript code for Miniproyectos project

---
+++ archivo.js
@@ -0,0 +1,18 @@
+document.write ("Miniproyectos.js");
+document.write ("Hola a todos.");
+
+var nombre; // Declaración de la variable tambien puede ser var nombre = "Juan";
+nombre = "Juan"; // Asignación de un valor a la variable
+var Nombre = "Maria"; // Declaración y asignación en una sola línea
+Var _nombre = "Carlos"; // Declaración de una variable con un guion bajo al inicio
+var NOMBRE = "Ana"; // Declaración de una variable con mayúsculas
+var $_nombre = "Luis"; // Declaración de una variable con un signo de dólar al inicio
+
+let saludo = "Hola"; // Declaración de una variable con let de modo local o global dependiendo del contexto
+let Saludo; // Declaración de la variable tambien puede ser var nombre = "Juan";
+let SALUDO = "como vas?"; // Asignación de un valor a la variable
+let _saludo = "Que tal?"; // Declaración de una variable con un guion bajo al inicio
+let $Saludo = "Holi"; // Declaración de una variable con un signo de dólar al inicio
+
+
+document.write ($Saludo);
